a4da2f14109ccdb024fdb025ccafc6f70f126e36
yohank <rlawjdgus843@gmail.com>
2022-08-06 16:23:41 +0900

    ADD inet_ntoa.c [네트워크 바이트 순서로 정렬된 정수형 IP주소 정보를 문자열의 형태로 변환] - 정수형태의 IP정보를 참조하여 문자열 형태의 IP정보로 변환하는 함수 inet_ntoa() ※ 함수호출 후에는 가급적 반환된 문자열 정보를 다른 메모리 공간에 복사해둘 것(inet_ntoa 함수의 반환형이 char형 포인터기 때문에 재호출되기 전까지만 반환된 문자열의 주솟값이 유효하다.)

---
+++ inet_ntoa.c
@@ -0,0 +1,34 @@
+//
+// Created by kimjeonghyeon on 2022-08-06.
+//
+#include <stdio.h>
+#include <string.h>
+#include <winsock2.h>
+
+int main(int argc, char *argv[])
+{
+    struct sockaddr_in addr1, addr2;
+    char *str_ptr;
+    char str_arr[20];
+
+    addr1.sin_addr.s_addr=htonl(0x1020304);
+    addr2.sin_addr.s_addr=htonl(0x1010101);
+
+    str_ptr=inet_ntoa(addr1.sin_addr);
+    strcpy(str_arr, str_ptr);
+    printf("Dotted-Decimal notation1: %s \n", str_ptr);
+
+    inet_ntoa(addr2.sin_addr);
+    printf("Dotted-Decimal notation2: %s \n", str_ptr);
+    printf("str_arr sizeof: %d \n", sizeof(str_arr));
+    printf("str_arr[0]: %c \n", str_arr[0]); // 1
+    printf("str_arr[1]: %c \n", str_arr[1]); // .
+    printf("str_arr[2]: %c \n", str_arr[2]); // 2
+    printf("str_arr[3]: %c \n", str_arr[3]); // .
+    printf("str_arr[4]: %c \n", str_arr[4]); // 3
+    printf("str_arr[5]: %c \n", str_arr[5]); // .
+    printf("str_arr[6]: %c \n", str_arr[6]); // 4
+    printf("str_arr[7]: %c \n", str_arr[7]); //
+    printf("str_arr[8]: %c \n", str_arr[8]); // ?
+    return 0;
+}
